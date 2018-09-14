b83947f77ecf4819ec860d5ab5e812fed7e5ccf6
Exiro <exirodev@gmail.com>
2018-09-14 19:55:49 +0200

    Ajout de la possibilitéde construire les stocks
      -rappel des touche
        -r : route
        -c : maison
        -s : stock

--- src/com/exiro/BuildingList/WaterWell.java
+++ src/com/exiro/BuildingList/WaterWell.java
@@ -2,6 +2,7 @@ package com.exiro.BuildingList;
 
 import com.exiro.Object.Case;
 import com.exiro.Object.City;
+import com.exiro.SystemCore.GameManager;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
@@ -9,6 +10,9 @@ import java.util.ArrayList;
 public class WaterWell extends Building {
 
 
+    public static WaterWell DEFAULT() {
+        return new WaterWell(false, 0, 0, 0, null, false, GameManager.currentCity);
+    }
     public WaterWell(boolean isActive, int pop, int xPos, int yPos, ArrayList<Case> cases, boolean built, City city) {
         super(isActive, BuildingType.WATERWELL, "Assets/Building/Waterfall/DefaultWaterfall.png", 118, 94, 2, BuildingCategory.FOOD, pop, 2, 50, 10, xPos, yPos, 2, 2, cases, built, city, 0);
     }
